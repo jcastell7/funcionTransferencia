5c69eff3d7fb2a97575720761ce44f3b123304ed
juan <jtcp27031@gmail.com>
2018-11-13 00:40:11 -0500

    [feat]: geometric place added

--- LugarGeometrico.m
+++ LugarGeometrico.m
@@ -1,35 +1,35 @@
-function varargout = GUI(varargin)
-% GUI MATLAB code for GUI.fig
-%      GUI, by itself, creates a new GUI or raises the existing
+function varargout = LugarGeometrico(varargin)
+% LugarGeometrico MATLAB code for LugarGeometrico.fig
+%      LugarGeometrico, by itself, creates a new LugarGeometrico or raises the existing
 %      singleton*.
 %
-%      H = GUI returns the handle to a new GUI or the handle to
+%      H = LugarGeometrico returns the handle to a new LugarGeometrico or the handle to
 %      the existing singleton*.
 %
-%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in GUI.M with the given input arguments.
+%      LugarGeometrico('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in LugarGeometrico.M with the given input arguments.
 %
-%      GUI('Property','Value',...) creates a new GUI or raises the
+%      LugarGeometrico('Property','Value',...) creates a new LugarGeometrico or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before GUI_OpeningFcn gets called.  An
+%      applied to the LugarGeometrico before LugarGeometrico_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to GUI_OpeningFcn via varargin.
+%      stop.  All inputs are passed to LugarGeometrico_OpeningFcn via varargin.
 %
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      *See LugarGeometrico Options on GUIDE's Tools menu.  Choose "LugarGeometrico allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help GUI
+% Edit the above text to modify the response to help LugarGeometrico
 
-% Last Modified by GUIDE v2.5 11-Nov-2018 21:30:21
+% Last Modified by GUIDE v2.5 13-Nov-2018 00:38:34
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @GUI_OpeningFcn, ...
-                   'gui_OutputFcn',  @GUI_OutputFcn, ...
+                   'gui_OpeningFcn', @LugarGeometrico_OpeningFcn, ...
+                   'gui_OutputFcn',  @LugarGeometrico_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before GUI is made visible.
-function GUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before LugarGeometrico is made visible.
+function LugarGeometrico_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to GUI (see VARARGIN)
+% varargin   command line arguments to LugarGeometrico (see VARARGIN)
 
-% Choose default command line output for GUI
+% Choose default command line output for LugarGeometrico
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes GUI wait for user response (see UIRESUME)
+% UIWAIT makes LugarGeometrico wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = GUI_OutputFcn(hObject, eventdata, handles) 
+function varargout = LugarGeometrico_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -113,76 +113,27 @@ Us = sym2poly(symDen);
 G = tf (Us,Ys);
 gprint = evalc ('G');
 set (handles.lblEq, 'string', gprint);
-[z,gain] = zero(G);
-ganancia = num2str(gain);
 [polos, ceros] = pzmap(Us,Ys);
+ramas = size(polos, 1);
+numceros = size(ceros,1);
 strPolos = mat2str(polos);
 strCeros = mat2str(ceros);
-stable = isstable(G);
-if stable
-    set (handles.lblEstabilidad ,'string' , 'El sistema es estable')
-else
-    set (handles.lblEstabilidad ,'string' , 'El sistema es inestable')
-end
+strRamas = mat2str(ramas);
+NumCeros =(ramas-numceros);
+absoluto = abs(NumCeros);
 set (handles.lblPolo, 'string',strPolos);
 set (handles.lblCero, 'string',strCeros);
-set (handles.lblGanancia, 'string',ganancia);
-tipoAmortiguamiento(Ys,handles);
-setappdata(0,'Us',Us);
-setappdata(0,'Ys',Ys);
-PolosYCeros();
-setappdata(0,'G',G);
-Escalon();
-Rampa();
+set (handles.lblRamas, 'string',strRamas);
+set (handles.lblNumCeros, 'string',absoluto);
+
+
+pzmap(Us,Ys);
+
+
+
+
+
 %end: function
-
-function tipoAmortiguamiento(Ys,handles)
-salida = Ys;
-
-if salida~=1
-    salida1 = salida/salida(1);
-else
-    salida1 = salida;
-end
-Wn=sqrt(salida1(3));
-Z=(salida1(2)/(2*Wn));
-if (Z<0)
-    amort = 'El Sistema Presenta Amortiguamiento Negativo';
-else
-    if (Z==0)
-        amort = 'El Sistema No Presenta Amortiguamiento';
-    else
-        if (Z==1)
-            amort = 'El Sistema Tiene un Amortiguamiento Critico';
-        else
-            if (Z>1)
-                amort = 'El Sistema es Sobreamortiguado';
-            else
-                if (0<=Z<=1)
-                    amort = 'El Sistema es Subamortiguado';
-                    
-                    %Tiempo de Asentamiento
-                    if (0<Z<0.69)
-                        Ts=((3.2)/(Z*Wn));
-                    end
-                    
-                    if (Z>0.69)
-                        Ts=((4.53)/(Wn));
-                    end
-                    %Sobrepaso Porcentual
-                    Sp=((100)*(exp((-pi*Z)/(sqrt(1-Z^2)))));
-
-                end
-            end
-        end
-    end
-end
-set (handles.lblAmortiguamiento, 'string',amort);
-strTs = num2str(Ts);
-set (handles.lblAsentamiento, 'string',strTs);
-strSp = num2str(Sp);
-set (handles.lblSobrepaso, 'string',strSp);
-
 
 
 
@@ -250,4 +201,27 @@ function slider1_CreateFcn(hObject, eventdata, handles)
 % Hint: slider controls usually have a light gray background.
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+function lblRamas_Callback(hObject, eventdata, handles)
+% hObject    handle to lblRamas (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of lblRamas as text
+%        str2double(get(hObject,'String')) returns contents of lblRamas as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function lblRamas_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to lblRamas (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
 end
